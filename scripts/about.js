499332cf559aa4f8cf43ca0b8eccc17dbccaa045
ALfredoAndrade14 <alfredo.andrade@ccc.ufcg.edu.br>
2020-11-03 09:09:02 -0300

    mudança na pagina sobre

---
+++ scripts/about.js
@@ -0,0 +1,13 @@
+function Texto1() {
+    document.getElementsByTagName("h1")[0].innerHTML = "O QUE É O CENTRO ACADÊMICO?";
+    document.getElementsByTagName("p")[1].innerHTML = "O Centro Acadêmico é uma entidade que representa todos os estudantes de um curso. Por exemplo, Cabe ao C.A., entre suas tarefas, realizar as discussões com os estudantes do curso para encontrar soluções nos problemas enfrentados, seja na relação com os professores, temas vinculados aos conteúdos e currículos dos cursos ou mesmo questões administrativas.";
+    b1.style.fill = "#A22522";
+    b2.style.fill = "gray";
+}
+
+function Texto2() {
+    document.getElementsByTagName("h1")[0].innerHTML = "SOBRE NÓS";
+    document.getElementsByTagName("p")[1].innerHTML = "Centro Acadêmico de Ciência da Computação é uma Organização sem fins lucrativos, a sua finalidade é representar os alunos do curso de Ciência da Computação da Universidade Federal de Campina Grande. Organizar, auxiliar e incentivar promoções de caráter político, cultural, científico e social, numa perspectiva de integração e formação.";
+    b1.style.fill = "gray";
+    b2.style.fill = "#A22522";
+}
